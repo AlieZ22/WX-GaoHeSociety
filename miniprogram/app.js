a1337eb65471fea4edbee4041ae9f6048ed12cfa
Zhimin Zeng <820460891@qq.com>
2020-03-11 17:17:43 +0800

    enhance the function of wode.js - auto get userInfo

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,6 +2,35 @@
 
 App({
   onLaunch: function () {
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+
+
     let that = this
     // 云开发初始化
     if (!wx.cloud) {
@@ -25,10 +54,11 @@ App({
       })
     
     }
+  },
 
-    this.globalData = {
-      _openid:"",
-      ne:{}
-    }
+  globalData:{
+    _openid:"",
+      ne:null,
+      userInfo:null
   }
 })
--- miniprogram/pages/wode/wode.js
+++ miniprogram/pages/wode/wode.js
@@ -1,23 +1,53 @@
 // pages/wode/wode.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    motto:"hello world",
+    userInfo:{},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   fuwu: function () {
     wx.navigateTo({
       url: "../xunxi_fuwu/xunxi_fuwu"
     })
   },
+  
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
   },
 
   /**
@@ -67,5 +97,13 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   }
 })
