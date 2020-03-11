9215eacdf65c81d5078c31c2be5b7dbea403f8e6
Zhimin Zeng <820460891@qq.com>
2020-03-11 18:04:09 +0800

    添加了报名判断，当前报名人数是否大于maxNum

--- miniprogram/pages/s_help/s_help.js
+++ miniprogram/pages/s_help/s_help.js
@@ -38,60 +38,87 @@ Page({
       return false
     }
     let that = this      // 处理异步请求
-    // 先将志愿者传到volunteers表中
-    DB.collection("hefuwu_volunteer").add({
-      data:{
-        contact:this.data.volunteer.contact,
-        name:this.data.volunteer.name,
-        location:this.data.volunteer.location,
-        hefuwu_id:this.data.initData._id,
-        state:0
-      },
+    // 要看是否超过报名该合服务的人数
+    DB.collection("hefuwu").doc(that.data.initData._id).get({
       success:function(res){
-        console.log("添加志愿者成功",res)
-        // 添加志愿者成功，得到volunteers表中该条记录的_id
-        // 获取该志愿者（这里从数据库中调，为了获取其_id和_openid）
-        DB.collection("hefuwu_volunteer").doc(res._id).get({
-          success:function(res){
-            // 然后调用云函数, 分别修改users表以及hefuwu表中的services, volunteers
-            wx.cloud.callFunction({
-              name:"add_userServices",
-              data:{
-                _openid: app.globalData._openid,
-                info:that.data.initData._id,
-                way:1  // 1表示为合服务志愿者（0表示合服务受助者）
-              },
-              success:function(res){
-                console.log("users表项添加完毕",res)
-              }
-            })
+        let currentNum = res.data.volunteers.length
+        let maxNum = res.data.maxVoluNum
+        if(currentNum<maxNum){
+          console.log("可以添加志愿者")
+          // 先将志愿者传到volunteers表中
+          DB.collection("hefuwu_volunteer").add({
+            data:{
+              contact:that.data.volunteer.contact,
+              name:that.data.volunteer.name,
+              location:that.data.volunteer.location,
+              hefuwu_id:that.data.initData._id,
+              state:0
+            },
+            success:function(res){
+              console.log("添加志愿者成功",res)
+              // 添加志愿者成功，得到volunteers表中该条记录的_id
+              // 获取该志愿者（这里从数据库中调，为了获取其_id和_openid）
+              DB.collection("hefuwu_volunteer").doc(res._id).get({
+                success:function(res){
+                  // 然后调用云函数, 分别修改users表以及hefuwu表中的services, volunteers
+                  wx.cloud.callFunction({
+                    name:"add_userServices",
+                    data:{
+                      _openid: app.globalData._openid,
+                      info:that.data.initData._id,
+                      way:1  // 1表示为合服务志愿者（0表示合服务受助者）
+                    },
+                    success:function(res){
+                      console.log("users表项添加完毕",res)
+                    }
+                  })
 
-            wx.cloud.callFunction({
-              name:"add_fuwuvolunteer",   // 对应合服务中添加volunteers列表
-              data:{
-                volunteer_id:res.data._id,
-                hefuwuId:that.data.initData._id
-              },
-              success:function(res){
-                console.log("云函数添加志愿者成功",res)
-              },
-              fail:function(res){
-                console.log("云函数添加志愿者失败",res)
-              }
-            })
-          },
-          fail:function(res){
-            console.log("获取志愿者失败",res)
-          }
-        })
-      },
-      fail:function(res){
-        console.log("添加志愿者失败",res)
+                  wx.cloud.callFunction({
+                    name:"add_fuwuvolunteer",   // 对应合服务中添加volunteers列表
+                    data:{
+                      volunteer_id:res.data._id,
+                      hefuwuId:that.data.initData._id
+                    },
+                    success:function(res){
+                      console.log("云函数添加志愿者成功",res)
+                    },
+                    fail:function(res){
+                      console.log("云函数添加志愿者失败",res)
+                    }
+                  })
+
+                  wx.showModal({
+                    title:"提示",
+                    content:"报名成功！",
+                    success:function(res){
+                      wx.navigateTo({
+                        url: "../services/services"
+                      })
+                    }
+                  })
+
+                },
+                fail:function(res){
+                  console.log("获取志愿者失败",res)
+                }
+              })
+            },
+            fail:function(res){
+              console.log("添加志愿者失败",res)
+            }
+          })
+        }else{
+          wx.showModal({
+            title:"提示",
+            content:"报名人数达到上限，报名失败！",
+            success:function(res){
+              wx.navigateTo({
+                url: "../services/services"
+              })
+            }
+          })
+        }
       }
-    })
-
-    wx.navigateTo({
-      url: "../msg_success/msg_success"
     })
   },
   // 获得输入
