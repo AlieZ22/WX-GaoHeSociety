a17a2c3004f04f7a9b672187324763f060537333
Tiana2018 <email@917803833@qq.com>
2020-03-15 00:38:09 +0800

    合服务-居民静态页面

--- miniprogram/pages/xunxi_fuwu/xunxi_fuwu.js
+++ miniprogram/pages/xunxi_fuwu/xunxi_fuwu.js
@@ -1,24 +1,48 @@
 // pages/xunxi_fuwu/xunxi_fuwu.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    meihaoList: []
+    navState: 0,   //导航状态,
+    FuwuList: [],
+    user: '',
+    user_id: '',
   },
-  // 获取云端数据(合美好)
-  getMeihaoData: function () {
+  //监听滑块
+  bindchange(e) {
+    // console.log(e.detail.current)
+    let index = e.detail.current;
+    this.setData({
+      navState: index
+    })
+  },
+  //点击导航
+  navSwitch: function (e) {
+    // console.log(e.currentTarget.dataset.index)
+    let index = e.currentTarget.dataset.index;
+    this.setData({
+      navState: index
+    })
+  },
+  // 获取云端数据(合服务)
+  getFuwuData: function () {
     let that = this   // 防止异步处理造成this指向改变
     wx.cloud.callFunction({
-      name: "get_meihaodata",
+      name: "get_fuwudata",
       success: function (res) {
         console.log("小程序获取数据", res)
-        that.setData({ meihaoList: res.result.data })
-        console.log(that.data.meihaoList)
+        that.setData({ fuwuList: res.result.data })
+        console.log(that.data.fuwuList)
+        that.setData({ user: app.globalData.user })
+        console.log(that.data.user)
+        that.setData({ user_id: app.globalData._openid })
+        console.log(that.data.user_id)
       },
       fail: function (res) {
-        console.log("小程序获取美好数据失败", res)
+        console.log("小程序获取服务数据失败", res)
       }
     })
   },
@@ -26,7 +50,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getMeihaoData()
+    this.getFuwuData()
+    var that = this;
+
   },
 
   /**
