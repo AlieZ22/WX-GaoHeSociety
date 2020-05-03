895bd60951017bc3c0908dd3b9d6c6b6304c8d7d
Tiana2018 <email@917803833@qq.com>
2020-05-04 00:26:22 +0800

    hejob

--- miniprogram/pages/wode/wode.js
+++ miniprogram/pages/wode/wode.js
@@ -32,6 +32,11 @@ Page({
       url: "../fabu_djInfo/fabu_djInfo"
     })
   },
+  zhaopin: function () {
+    wx.navigateTo({
+      url: "../fabu_zpInfo/fabu_zpInfo"
+    })
+  },
 
   /**
    * 生命周期函数--监听页面加载
